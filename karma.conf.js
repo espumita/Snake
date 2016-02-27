f879b42502c711339fcb9ee72cbc7c4b1c31f640
David Falcón Naranjo <david_el_125@hotmail.com>
2016-02-27 13:34:19 +0000

    karma base path set to current project directory

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
     config.set({
 
         // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: 'C:/Users/David/WebstormProjects/snake',
+        basePath: '.',
 
 
         // frameworks to use

a1324de186627356cbe529d78cbb1c9725af810c
David Falcón Naranjo <david_el_125@hotmail.com>
2016-02-27 13:25:37 +0000

    Jasmine test framework changed to mocha, and adding Chai assertion library, using karma as test runner

--- test/_Snake.js
+++ test/_Snake.js
@@ -1,5 +1,8 @@
-describe('Unit: suit try', function() {
-    it('shoud true', function() {
-        expect(true).toBe(true);
+describe('Array', function() {
+    it('when_true_should_be_true', function () {
+        expect(true).to.be.true;
+    });
+    it('when_string_equal_to_string',function(){
+         assert.equal("a","a");
     });
 });
